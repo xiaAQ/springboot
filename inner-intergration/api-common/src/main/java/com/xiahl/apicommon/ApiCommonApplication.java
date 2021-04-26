26a3768534a7632b022520318fc4beac7ce0e8ff
夏浩磊 <xiahl@yczcjk.com>
2021-04-26 16:04:49 +0800

    refactor:项目模块搭建 增加公共模块

---
+++ inner-intergration/api-common/src/main/java/com/xiahl/apicommon/ApiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xiahl.apicommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCommonApplication.class, args);
+    }
+
+}
