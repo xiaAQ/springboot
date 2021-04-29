1ccf7bbad12acd6738b5040997149084de41c3d6
夏浩磊 <xiahl@yczcjk.com>
2021-04-29 10:43:56 +0800

    refactor:增加定时任务模块

---
+++ job-center/src/test/java/com/xiahl/job/JobCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiahl.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
