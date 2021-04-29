ec96730e50bfb647bb53b8060c6d25f09f1b27f6
夏浩磊 <xiahl@yczcjk.com>
2021-04-29 08:23:17 +0800

    refactor:增加模块  springsecurity

--- web-protal/back-center/src/main/java/com/xiahl/web/controller/LoginController.java
+++ web-protal/back-center/src/main/java/com/xiahl/web/controller/LoginController.java
@@ -1,11 +1,19 @@
 package com.xiahl.web.controller;
 
 
+import ch.qos.logback.core.boolex.EvaluationException;
+import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-
+    @GetMapping("/userlogin")
+    @ApiOperationSupport(author = "xiahl")
+    public String login(String userName, String password) {
+        return "登陆账号和密码为"+userName + password;
+    }
 }
